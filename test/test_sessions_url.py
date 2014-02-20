74d2515408828a2121db047b76042313d210a04c
JessicaBachmann <Jessica.Bachmann@hhu.de>
2014-02-21 00:49:15 +0100

    testcase for session url

--- test/test_sessions_url.py
+++ test/test_sessions_url.py
@@ -4,35 +4,41 @@ import sys
 import tutil
 import webvulnscan.attacks.session_url
 
-#Session ID 32 characters mostly with GET method POST also possible
-
+#creating site without any session
 def make_client(headers):
     headers['Content-Type'] = 'text/html; charset=utf-8'
     return tutil.TestClient({
-        '/':(200, b'<html></html>, headers),
+        '/':(200, b'<html></html>', headers),
     })
-
+    
 
 class SessionUrl(unittest.TestCase):
+    headers = "Set-Cookie" : "random=test"
     def test_static_site(self):
         client = make_client({})
         client.run_attack(webvulnscan.attacks.session_url)
         client.log.assert_count(0)
 
-    #test site with session id in URI POST
-    def test_site_with_id(self):
-        client = make_client({
-            "Set-Cookie" : "random = test"
-            "Method" : "POST"
+    #test site/form with session id in URI POST
+    def test_site_with_post(self):
+        client = tutil.TestClient({
+            '/': u'''<html>
+                <form method = POST>
+                    <input type="submit" />
+                </form>
+                </html>''', headers
         })
         client.run_attack(webvulnscan.attacks.session_url)
         client.log.assert_count(1)
         
-    #test site with session id in URI GET
-    def test_site_with_id(self):
+    #test site/form with session id in URI GET
+    def test_site_with_get(self):
         client = make_client({
-            "Set-Cookie" : "random = test"
-            "Method" : "GET"
+            '/': u'''<html>
+                <form method = GET>
+                    <input type="submit" />
+                </form>
+                </html>''', headers
         })
         client.run_attack(webvulnscan.attacks.session_url)
         client.log.assert_count(1)
