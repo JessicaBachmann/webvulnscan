8573ed0d01c7026168950f1bf6d39799e1749fde
JessicaBachmann <Jessica.Bachmann@hhu.de>
2014-03-06 00:39:55 +0100

    add session_url to __init__.py

--- webvulnscan/attacks/__init__.py
+++ webvulnscan/attacks/__init__.py
@@ -6,7 +6,8 @@ from .breach import breach
 from .clickjack import clickjack
 from .cookiescan import cookiescan
 from .exotic_characters import exotic_characters
+from .session_url import session_url
 
 
 def all_attacks():
-    return [xss, csrf, crlf, breach, clickjack, cookiescan, exotic_characters]
+    return [xss, csrf, crlf, breach, clickjack, cookiescan, exotic_characters, session_url]
