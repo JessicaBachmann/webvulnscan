f31b3776e2ac68e0b5daa0739f28d80e90837074
JessicaBachmann <Jessica.Bachmann@hhu.de>
2014-03-06 01:05:43 +0100

    attacker for session url

--- webvulnscan/attacks/session_url.py
+++ webvulnscan/attacks/session_url.py
@@ -1,16 +1,19 @@
 from ..utils import attack
 
 
+def check_id(page):
+    if "sid" in page.url:
+        return true
+    if "sessionid" in page.url:
+        return true
+    if "phpsessid" in page.url:
+        return true
+    return false
 
 
-
-
-def attack_get(headers):
-
-
-
-def attack_post(headers):
-
-
-@attck()
-def session_url(client, log, page)
+@attack()
+def session_url(client, log, page):
+    #session id in url found
+    if check_id(page):
+        log('vuln', page.url, u"Session ID in URL")
+    return
